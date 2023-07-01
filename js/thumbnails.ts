0b5f6cef3e0213aa2198e279e723eef031a3bfcd
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-07-01 10:20:50 +0300

    Open big photo this universal click handler

--- js/thumbnails.ts
+++ js/thumbnails.ts
@@ -11,19 +11,26 @@ if (!template || !picturesWrapper) {
 
 const fragment = document.createDocumentFragment();
 
-const renderPicture = (photo: Photo) => {
-	const { url, description, likes, comments } = photo;
+const onThumbnailClick = (evt: Event) => {
+	evt.preventDefault();
+	const link = evt.currentTarget as HTMLAnchorElement;
+	const id = Number(link.dataset.id);
+	const foundPhoto = photos.find((photo) => photo.id === id);
+
+	if (foundPhoto) {
+		return openPhoto(foundPhoto);
+	}
+};
+
+const renderPicture = ({id, url, description, likes, comments }: Photo) => {
 	const pictureElement = template.cloneNode(true) as HTMLAnchorElement;
 	const pictureTag = pictureElement.querySelector<HTMLImageElement>('.picture__img');
 	if (!pictureTag) {
 		return;
 	}
-	pictureElement.addEventListener('click', (evt) => {
-		evt.preventDefault();
-		openPhoto(photo);
-	})
 
-	// pictureElement.href = `photos/${id}`;
+	pictureElement.dataset.id = id.toString();
+	pictureElement.addEventListener('click', onThumbnailClick);
 
 	pictureTag.src = url;
 	pictureTag.alt = description;
