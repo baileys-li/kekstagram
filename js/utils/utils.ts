d4406e4de9f6dfb72303e7c0d3ee24ca79a3406a
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-07-01 11:15:08 +0300

    Write test for unique array util

--- js/utils/utils.ts
+++ js/utils/utils.ts
@@ -3,7 +3,6 @@ type EscapeKeyEvent = Omit<KeyboardEvent, 'key'> & { key: 'Escape' };
 
 const isEscapeKey = (evt: KeyboardEvent): evt is EscapeKeyEvent => evt.key === 'Escape';
 
-
 const isUniqueArray = <El>(array: El[] | readonly El[]) => new Set(array).size === array.length;
 
 export { isUniqueArray, isEscapeKey };
