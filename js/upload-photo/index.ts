9c8f67ff7ae41615d0041653c7ccd95aa414f337
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-07-18 22:35:48 +0300

    Add full validation

--- js/upload-photo/index.ts
+++ js/upload-photo/index.ts
@@ -1,11 +1,13 @@
 import { isEscapeKey, toggleModalClasses } from '../utils';
 import { form, wrapper } from './elements';
 import './validation';
+import { resetValidation, validate } from './validation';
 
 const closeForm = () => form!.reset();
 
 const onDocumentEscape = (evt: KeyboardEvent) => {
-	if (isEscapeKey(evt)) {
+	const isFocusOnText = document.activeElement === form!.hashtags || document.activeElement === form!.description;
+	if (isEscapeKey(evt) && !isFocusOnText) {
 		closeForm();
 	}
 };
@@ -17,6 +19,11 @@ form!.filename.addEventListener('change', () => {
 
 form!.addEventListener('reset', () => {
 	toggleModalClasses(wrapper!, false);
+	resetValidation();
 	document.removeEventListener('keydown', onDocumentEscape);
 });
 
+form!.addEventListener('submit', (evt) => {
+	evt.preventDefault();
+	validate();
+});
--- js/upload-photo/validation.ts
+++ js/upload-photo/validation.ts
@@ -1,31 +1,73 @@
 import Pristine from 'pristinejs';
 import { form } from './elements';
+import { isUniqueArray } from '../utils';
 
+const enum Hashtags {
+	MaxCount = 5,
+	MaxCountError = 'Нельзя указать больше пяти хэш-тегов',
+	UniqueError = 'Один и тот же хэш-тег не может быть использован дважды',
+	HashStartError = 'Хэштег должен начинаться с #',
+	LengthError = 'Максимальная длина одного хэш-тега 20 символов, включая решётку',
+	CharactersError = 'Строка после решётки должна состоять из букв и чисел и не может содержать пробелы спецсимволы и т п символы пунктуации тире дефис запятая и т п эмодзи и т д',
+	OnlyHashError = 'Хеш-тег не может состоять только из одной решётки',
+}
 
+const HASHTAG_REG_EXP = /^#[a-zа-яё0-9]{1,19}$/;
 
 export const pristine = new Pristine(form!, {
 	classTo: 'img-upload__field-wrapper',
-	errorTextParent: 'img-upload__field-wrapper'
+	errorTextParent: 'img-upload__field-wrapper',
 });
 
 let hashtagsError = '';
 
 const validateHashtags = (value: string) => {
-	console.log(value);
+	/** @hint потому что не required */
+	if (value.length === 0) {
+		return true;
+	}
 
-	if (value === '2') {
-		hashtagsError = 'Custom error message';
+	const tags = value.trim().toLocaleLowerCase().split(' ');
+
+	if (tags.length > Hashtags.MaxCount) {
+		hashtagsError = Hashtags.MaxCountError;
 		return false;
 	}
 
-	if (value === '3') {
-		hashtagsError = 'Anoter custom error message';
+	if (!isUniqueArray(tags)) {
+		hashtagsError = Hashtags.UniqueError;
 		return false;
 	}
 
-	return true;
-}
+	return tags.every((tag) => {
+		if (tag[0] !== '#') {
+			hashtagsError = Hashtags.HashStartError;
+			return false;
+		}
+
+		if (tag.length > 20) {
+			hashtagsError = Hashtags.LengthError;
+			return false;
+		}
+
+		if (tag === '#') {
+			hashtagsError = Hashtags.OnlyHashError;
+			return false;
+		}
+
+		if (!HASHTAG_REG_EXP.test(tag)) {
+			hashtagsError = Hashtags.CharactersError;
+			return false;
+		}
+
+
+		return true;
+	});
+};
 
 pristine.addValidator(form!.hashtags, validateHashtags, () => hashtagsError);
 
+const validate = () => pristine.validate();
+const resetValidation = () => pristine.reset();
 
+export { validate, resetValidation };
