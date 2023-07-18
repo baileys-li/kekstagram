03d276c5c42f91471a937c4296e9bcbdd4067d33
Arthur Baileys Li <arthurlitovko@gmail.com>
2023-07-18 22:35:35 +0300

    Add simple example of Pristine work

--- js/upload-photo/validation.ts
+++ js/upload-photo/validation.ts
@@ -1,3 +1,31 @@
-import Pristine from 'pristinejs'
+import Pristine from 'pristinejs';
+import { form } from './elements';
 
-console.log(Pristine);
+
+
+export const pristine = new Pristine(form!, {
+	classTo: 'img-upload__field-wrapper',
+	errorTextParent: 'img-upload__field-wrapper'
+});
+
+let hashtagsError = '';
+
+const validateHashtags = (value: string) => {
+	console.log(value);
+
+	if (value === '2') {
+		hashtagsError = 'Custom error message';
+		return false;
+	}
+
+	if (value === '3') {
+		hashtagsError = 'Anoter custom error message';
+		return false;
+	}
+
+	return true;
+}
+
+pristine.addValidator(form!.hashtags, validateHashtags, () => hashtagsError);
+
+
